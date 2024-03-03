e77ba1f8d047f69cdf0733af63c6e151b9cf5c86
mentalisit <mentalisit@gmail.com>
2024-03-03 17:01:26 -0600

    1.0.0.1

--- Timers.go
+++ Timers.go
@@ -1,9 +1,9 @@
 package models
 
 type Timer struct {
-	Dsmesid  string `bson:"dsmesid"`
-	Dschatid string `bson:"dschatid"`
-	Tgmesid  string `bson:"tgmesid"`
-	Tgchatid string `bson:"tgchatid"`
-	Timed    int    `bson:"timed"`
+	DsMesId  string `json:"ds_mes_id"`
+	DsChatId string `json:"ds_chat_id"`
+	TgMesId  string `json:"tg_mes_id"`
+	TgChatId string `json:"tg_chat_id"`
+	Timed    int    `json:"timed"`
 }
--- rsBot.go
+++ rsBot.go
@@ -12,3 +12,14 @@ type ToRsMessage struct {
 	Guildid     string `json:"guildid"`
 	Avatar      string `json:"avatar"`
 }
+type CorporationConfig struct {
+	CorpName    string `json:"corp_name"`
+	DsChannel   string `json:"ds_channel"`
+	TgChannel   string `json:"tg_channel"`
+	Language    string `json:"language"`
+	MesIdDsHelp string `json:"mes_id_ds_help"`
+	MesIdTgHelp string `json:"mes_id_tg_help"`
+	Forward     bool   `json:"forward"`
+	DelMessage  bool   `json:"del_message"`
+	GuildId     string `json:"guild_id"`
+}
